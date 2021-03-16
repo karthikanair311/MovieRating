1854b8ed3713f4c5259f8b61576ecea5a31d0416
Karthika Nair <karthikanair311@gmail.com>
2021-03-16 21:53:17 +0530

    migration reviewer updated

--- MovieRating/Migrations/20210224145816_ReviewerDetails.cs
+++ MovieRating/Migrations/20210224145816_ReviewerDetails.cs
@@ -6,8 +6,8 @@ namespace MovieRating.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Reviewer");
+            //migrationBuilder.DropTable(
+            //    name: "Reviewer");
 
             migrationBuilder.CreateTable(
                 name: "ReviewerDetails",
@@ -29,18 +29,18 @@ namespace MovieRating.Migrations
             migrationBuilder.DropTable(
                 name: "ReviewerDetails");
 
-            migrationBuilder.CreateTable(
-                name: "Reviewer",
-                columns: table => new
-                {
-                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    Age = table.Column<int>(type: "int", nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Reviewer", x => x.Id);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "Reviewer",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+            //        Age = table.Column<int>(type: "int", nullable: false),
+            //        Name = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_Reviewer", x => x.Id);
+            //    });
         }
     }
 }
